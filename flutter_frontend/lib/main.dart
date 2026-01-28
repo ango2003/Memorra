e5f45a0ea66c09e736a50470d61ae94449c1a940
Aaron Ngo <ango1@atu.edu>
2026-01-27 20:58:16 -0600

    Firebase Authentication Initialization

--- flutter_frontend/lib/main.dart
+++ flutter_frontend/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,10 +21,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Firebase Connected!'),
+          child: Container(
+            width: 250,
+            height: 140,
+            color: Colors.green,
+            padding: const EdgeInsets.all(8.0)
+          ),
         ),
       ),
     );
