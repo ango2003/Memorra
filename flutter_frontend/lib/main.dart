aacb24a4d7512188cbe2beeb0d1e7f37706178bc
Aaron Ngo <ango1@atu.edu>
2026-02-01 19:18:44 -0600

    Simple Authentication Code

--- flutter_frontend/lib/main.dart
+++ flutter_frontend/lib/main.dart
@@ -11,7 +11,19 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
+  // Initialization of Database
   final FirebaseFirestore db = FirebaseFirestore.instance;
+
+  // Simple Authentication detection
+  FirebaseAuth.instance
+  .userChanges()
+  .listen((User? user) {
+    if (user == null) {
+      print('User is currently signed out!');
+    } else {
+      print('User is signed in!');
+    }
+  });
 
   runApp(const MainApp());
 }
@@ -22,16 +34,31 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Container(
-            width: 250,
-            height: 140,
-            color: Colors.green,
-            padding: const EdgeInsets.all(8.0)
-          ),
-        ),
-      ),
+    title: 'Memorra Test Page',
+      // The StreamBuilder goes here as the "home" of your app
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
+          if (snapshot.hasError) {
+            return const Scaffold(body: Center(child: Text('Something went wrong')));
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(body: Center(child: Text("Loading...")));
+          }
+
+          // If there is no user data, show the Sign In screen
+          if (!snapshot.hasData) {
+            //No screens yet
+            //return const SignInScreen();
+          }
+
+          // If there is a user, show the Home screen
+          final user = snapshot.data!;
+          //No screens yet
+          //return HomeScreen(userId: user.uid);
+        },
+      )
     );
   }
 }
