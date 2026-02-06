7a128358c410dc24325c7f2e6d3aaced2b9970f0
Aaron Ngo <ango1@atu.edu>
2026-02-05 23:18:20 -0600

    File for Authentication Function

--- flutter_frontend/lib/main.dart
+++ flutter_frontend/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
@@ -34,31 +35,31 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    title: 'Memorra Test Page',
-      // The StreamBuilder goes here as the "home" of your app
-      home: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
-          if (snapshot.hasError) {
-            return const Scaffold(body: Center(child: Text('Something went wrong')));
-          }
+    // title: 'Memorra Test Page',
+    //   // The StreamBuilder goes here as the "home" of your app
+    //   home: StreamBuilder<User?>(
+    //     stream: FirebaseAuth.instance.authStateChanges(),
+    //     builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
+    //       if (snapshot.hasError) {
+    //         return const Scaffold(body: Center(child: Text('Something went wrong')));
+    //       }
 
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Scaffold(body: Center(child: Text("Loading...")));
-          }
+    //       if (snapshot.connectionState == ConnectionState.waiting) {
+    //         return const Scaffold(body: Center(child: Text("Loading...")));
+    //       }
 
-          // If there is no user data, show the Sign In screen
-          if (!snapshot.hasData) {
-            //No screens yet
-            //return const SignInScreen();
-          }
+    //       // If there is no user data, show the Sign In screen
+    //       if (!snapshot.hasData) {
+    //         //No screens yet
+    //         //return const SignInScreen();
+    //       }
 
-          // If there is a user, show the Home screen
-          final user = snapshot.data!;
-          //No screens yet
-          //return HomeScreen(userId: user.uid);
-        },
-      )
+    //       // If there is a user, show the Home screen
+    //       final user = snapshot.data!;
+    //       //No screens yet
+    //       //return HomeScreen(userId: user.uid);
+    //     },
+    //   )
     );
   }
 }
