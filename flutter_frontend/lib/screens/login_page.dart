bb7e94a5783a3404685a229c433494ee3d5e1438
Aaron Ngo <ango1@atu.edu>
2026-02-15 19:57:18 -0600

    Sign up functionality

--- flutter_frontend/lib/screens/login_page.dart
+++ flutter_frontend/lib/screens/login_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LogInState();
+}
+
+class _LogInState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
