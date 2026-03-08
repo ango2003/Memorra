2c93425fd9c82cb7ed5591eebaefc28c34403e4f
cashmoney121712 <cashmoney121712@gmail.com>
2026-03-08 00:17:46 -0600

    Home Page ReDesign + Nav Bar Updates (#21)
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit

--- flutter_frontend/lib/screens/home_page.dart
+++ flutter_frontend/lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'widgets/home_tile.dart';
 import 'widgets/nav_bar.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,24 +9,119 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home Page")),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text('Welcome! Your UID is: $userId'),
-            SizedBox(height: 8),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/profilepage');
-              },
-              child: Text("Go To Profile"),
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: Theme.of(context).brightness == Brightness.dark
+                  ? [
+                      Color(0xFF0A6F85),
+                      Color(0xFF071F4A),
+                    ]
+                  : [
+                      Color(0xFF3F6BB5),
+                      Color(0xFF0A8C7A),
+                    ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
-          ],
+          ),
+
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(height: 60),
+
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                child: Text(
+                  "How Can We Help " + userId + "?",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 75,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).brightness == Brightness.dark
+                        ? Colors.white
+                        : Color(0xFF071F4A),
+                  ),
+                ),
+              ),
+
+              Divider(
+                color: Theme.of(context).brightness == Brightness.dark
+                    ? Colors.white
+                    : Colors.black54,
+                thickness: 5,
+                indent: 20,
+                endIndent: 20,
+              ),
+              SizedBox(height: 50),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: HomeTile(
+                            title: "Today's Reminders",
+                            onTap: () {
+                              Navigator.pushNamed(context, '/wip');
+                            },
+                          ),
+                        ),
+                        SizedBox(width: 20),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: HomeTile(
+                            title: "Suggested Reminders",
+                            onTap: () {
+                              Navigator.pushNamed(context, '/wip');
+                            },
+                          ),
+                        ),
+                        SizedBox(width: 20),
+                        Expanded(
+                          child: HomeTile(
+                            title: "Friend Gift Ideas",
+                            onTap: () {
+                              Navigator.pushNamed(context, '/wip');
+                            },
+                          ),
+                        ),
+                        SizedBox(width: 20),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: HomeTile(
+                            title: "Set New Reminder",
+                            onTap: () {
+                              Navigator.pushNamed(context, '/wip');
+                            },
+                          ),
+                        ),
+                        SizedBox(width: 20),
+                      ],
+                    ),
+                    SizedBox(height: 40),
+                  ],
+                ),
+              )
+            ],
+          )
         ),
       ),
       bottomNavigationBar: NavBar(
-        currentIndex: 2
+        currentIndex: 2,
       ),
     );
   }
